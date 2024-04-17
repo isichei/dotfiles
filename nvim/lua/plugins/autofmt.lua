a4efa203006cfc1fd2a8091e0bff82d0d101a274
Karik Isichei <karikisichei@gmail.com>
2024-04-17 16:24:03 +0100

    Moving LSP to its own plugin

--- nvim/lua/plugins/autofmt.lua
+++ nvim/lua/plugins/autofmt.lua
@@ -1,8 +1,3 @@
--- autoformat.lua (from nvim kickstart, with some minor mods)
---
--- Use your language server to automatically format your code on save.
--- Adds additional commands as well to manage the behavior
-
 return {
   'neovim/nvim-lspconfig',
   config = function()
